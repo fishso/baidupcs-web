1fd8c83c40f609ecdba1be7445368684e793839a
minierliu <minierliu@zhen22.com>
2018-11-01 11:30:35 +0800

    增加ws协议判断，以方便使用反代

--- src/config/pcsconfig.js
+++ src/config/pcsconfig.js
@@ -2,7 +2,7 @@ var host = location.host;
 
 export default {
     base_url: '/', //production
-    ws_url: 'ws://' + host + '/ws', //production
+    ws_url: location.protocol.replace('^http','ws')+'//' + host + '/ws', //production
 
     // base_url: 'http://127.0.0.1:8081/', //develop
     // ws_url: 'ws://127.0.0.1:8081/ws', //develop
